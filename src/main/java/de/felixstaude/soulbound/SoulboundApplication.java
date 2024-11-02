420a7942771dfccbc65fe7fcda3474d0f10203e3
Felix Staude <felixstaude0112@gmail.com>
2024-11-03 00:27:21 +0100

    Initial commit

---
+++ src/main/java/de/felixstaude/soulbound/SoulboundApplication.java
@@ -0,0 +1,13 @@
+package de.felixstaude.soulbound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoulboundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoulboundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/felixstaude/soulbound/SoulboundApplicationTests.java
@@ -0,0 +1,13 @@
+package de.felixstaude.soulbound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoulboundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
